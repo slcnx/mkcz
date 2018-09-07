0ea876baed73604df3b7dc3e7a9a5af9a09ec2cc
liangcheng.song <liangcheng.song@example.com>
2018-09-07 14:42:30 +0800

    check iot port v4

--- iot-port-monitor/60_a.py
+++ iot-port-monitor/60_a.py
@@ -6,9 +6,51 @@ import json
 import os
 
 iot_dict = {
-  "io.device-ms": ['5001', '6001'],
-   "iot.userdevice-ms": ['5002', '6002']
-  }
+    "iotdevice-ms": ["5001", "6001"],
+    "iotuserdevice-ms": ["5002", "6002"],
+    "iothouse-ms": ["5003", "6003"],
+    "iotai-ms": ["5004", "6004"],
+    "iotpublic-ms": ["5005", "6005"],
+    "iotdata-ms": ["5006", "6006"],
+    "iotauth-ms": ["5007", "6007"],
+    "iotuser-ms": ["5008", "6008"],
+    "iotdns-ms": ["5009", "6009"],
+    "iotautoai-ms": ["5010", "6010"],
+   "iotyos-ms": ["5011", "8081", "6011"],
+    "iotevent-ms": ["5012", "6012"],
+    "iotonline-ms": ["5013", "6013"],
+    "iotpush-ms": ["5016", "6016"],
+    "iotpay-ms": ["5017", "6017"],
+    "iotdefense-ms": ["5018", "6018"],
+    "iotacl-ms": ["5032", "6032"],
+    "iotauthority-ms": ["5021", "6021"],
+    "iotauthority-master": ["5022", "6022"],
+    "datasync-master": ["5023", "6023"],
+    "apigw-ms": ["443", "1443", "5025", "6025"],
+    "iotgw-mq": ["1888", "1889"],
+    "mini-mq": ["2888", "2889"],
+    "yuemq-router": ["5888", "5889"],
+    "yuemq-cnode": ["6888", "6889"],
+    "local-mq": ["3888", "3889"],
+    "yuemq-global": ["10888", "10889"],
+    "iotconnnat-ms": ["2001", "3001"],
+    "iotconnp2p-ms": ["5999", "6999"],
+    "iotconnrelay-ms": ["5998", "6998"],
+    "iotlog-ms": ["5024", "6024"],
+    "iotorder-ms": ["5030", "6030"],
+    "iotoss-ms": ["5026", "6026"],
+    "iotidx-ms": ["5027", "6027"],
+    "iotgoods-ms": ["5033", "6033"],
+    "iotadmin-ms": ["5031", "8088", "6031"],
+    "iotinfrared-ms": ["5034", "6034"],
+    "iotmonitor-ms": ["5035", "6035"],
+    "iotoauth2-ms": ["5036", "6036"],
+    "iotalivoice-ms": ["5037", "6037"],
+    "iotdingdauth-ms": ["5038", "6038"],
+    "iotdingdvoice-ms": ["5039", "6039"],
+    "iotawsvoice-ms": ["5040", "6040"],
+    "iotggvoice-ms": ["5042", "6042"]
+    }
 
 def fetch_ss_state(k, port_list):
     count = 0
