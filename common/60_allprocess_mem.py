cfe0d554b120a2dacee81f1191fefa95b8f72347
pxr <mykernel@foxmail.com>
2019-07-26 06:34:22 -0300

    json dumps

--- common/60_allprocess_mem.py
+++ common/60_allprocess_mem.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from subprocess import Popen, PIPE
+import json
 import  time,os
 
 def get_names():
@@ -8,7 +9,7 @@ def get_names():
   p1 = Popen(["ss","-tanp"], stdout=PIPE)
   p2 = Popen(["grep", "-Po", "(?<=users:\(\(\")[\w-]+"], stdin=p1.stdout, stdout=PIPE)
   raw_data = Popen(["sort","-u"], stdin=p2.stdout, stdout=PIPE)
-  allProcesses = raw_data.communicate()[0].split()
+  allProcesses = raw_data.communicate()[0].decode().split()
   return allProcesses
 
 
@@ -33,8 +34,8 @@ def fetch_process_info(process):
   p2 = Popen(["grep", process], stdin=p1.stdout, stdout=PIPE)
   p3 = Popen(["grep","-v", "grep"], stdin=p2.stdout, stdout=PIPE)
   mem = Popen(["awk","{sum+=$4}END{print sum}"], stdin=p3.stdout, stdout=PIPE)
-  cpupercent = cpu.communicate()[0]
-  mempercent = mem.communicate()[0]
+  cpupercent = cpu.communicate()[0].decode()
+  mempercent = mem.communicate()[0].decode()
   create_record(process,'cpupercent',cpupercent)
   create_record(process,'mempercent',mempercent)
 
@@ -42,4 +43,4 @@ if __name__ == "__main__":
   data = []
   for process in get_names():
     fetch_process_info(process) 
-  print(data)
+  print(json.dumps(data))
